11fd78dcb85df40aff46a304177c942b3261ef0f
Max Shytikov <mshytikov@gmail.com>
2017-07-26 09:27:29 +0000

    Added pickling letters

--- lib/io.py
+++ lib/io.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 from scipy import ndimage
+import pickle
+import pathlib
 
 
 def normalize(image_data, pixel_depth=255.0):
@@ -9,7 +11,7 @@ def normalize(image_data, pixel_depth=255.0):
 
 def validate(image_data, image_size=28):
     if image_data.shape != (image_size, image_size):
-        error_msg = ('Unexpected image shape: %s'.format(image_data.shape))
+        error_msg = ('Unexpected image shape: {}'.format(image_data.shape))
         raise Exception(error_msg)
     return image_data
 
@@ -26,3 +28,21 @@ def load_images(image_files):
 def load_letter(src_dir):
     image_files = [os.path.join(src_dir, f) for f in os.listdir(src_dir)]
     return np.stack(load_images(image_files))
+
+
+def piclke_letter(src_dir, dest_file):
+    with open(dest_file, 'wb') as f:
+        dataset = load_letter(src_dir)
+        pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+
+
+def pickle_letters(src_dir=None, dest_dir=None):
+    pathlib.Path(dest_dir).mkdir(parents=True, exist_ok=True)
+    for letter in os.listdir(src_dir):
+        letter_dir = os.path.join(src_dir, letter)
+        letter_pickle_file = os.path.join(dest_dir, letter) + '.pickle'
+
+        if os.path.isfile(letter_pickle_file):
+            print("File already exists: {}".format(letter_pickle_file))
+        else:
+            piclke_letter(letter_dir, letter_pickle_file)
